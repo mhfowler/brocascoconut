89a7174059f6876931cd4f197bd9a8f1c4d31760
Max Fowler <maxhfowler@gmail.com>
2014-10-08 18:55:55 -0400

    mhfowler

--- mhf/views.py
+++ mhf/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
 from django import shortcuts
 from mhf.models import Stat
 from django.shortcuts import render
+import random, os
+from settings.common import PROJECT_PATH
 
-
+# boiler ###############################################################################################################
 def redirect(request, page='/home'):
     return shortcuts.redirect(page)
 
@@ -14,7 +15,8 @@ def viewWrapper(view):
     return new_view
 
 
-def home(request):
+# home #################################################################################################################
+def getNumVisitors():
     stat = Stat.xg.get_or_none(name="test")
     if not stat:
         stat = Stat(name="test")
@@ -22,15 +24,47 @@ def home(request):
     else:
         stat.number += 1
         stat.save()
-    return render(request, 'home.html', {"stat":stat})
-    # return shortcuts.redirect("/machine_learning/")
+    return stat
 
-# data science blog posts
+def home(request):
+    stat = getNumVisitors()
+    return render(request, 'home.html', {"stat":stat})
+
+
+# data science blog posts ##############################################################################################
 def machine_learning(request):
-    return render(request, 'cs195_assignment3.html', {})
+    return render(request, 'dataScienceClass/cs195_assignment3.html', {})
 
 def twitter_visualization(request):
-    return render(request, 'cs195_assignment4/writeup.html', {})
+    return render(request, 'dataScienceClass/cs195_assignment4/writeup.html', {})
 
 def map_reduce(request):
-    return render(request, 'map_reduce.html', {})
+    return render(request, 'dataScienceClass/map_reduce.html', {})
+
+
+# robert marvin ########################################################################################################
+
+def monkeySkull(request):
+    stat = getNumVisitors()
+    return render(request, 'monkeySkull.html', {"stat":stat})
+
+def loadingCrazy(request):
+    stat = getNumVisitors()
+    saying = getSaying()
+    return render(request, 'loadingCrazy.html', {"saying":saying})
+
+def getSaying():
+    sayings_file = os.path.join(PROJECT_PATH,"mhf/static/sayings.txt")
+    with open(sayings_file, "r") as f:
+        lines = []
+        for line in f:
+            line = line.replace("\n", "")
+            lines.append(line)
+        saying = random.choice(lines)
+    return saying
+
+
+def theHome(request):
+    stat = getNumVisitors()
+    return render(request, 'theHome.html', {"stat":stat})
+
--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-from mhf.views import viewWrapper, home, redirect, machine_learning, twitter_visualization,map_reduce
+from mhf.views import viewWrapper, home, redirect, machine_learning, twitter_visualization,map_reduce, monkeySkull, \
+loadingCrazy, theHome
 from settings.common import LOCAL, STATIC_URL, STATIC_ROOT
 from django.conf.urls.static import static
 from django.conf import settings
@@ -11,6 +12,9 @@ from django.conf import settings
 #
 # patterns for mhfowler
 urlpatterns = patterns('',
+                       (r'^______/$', viewWrapper(theHome)),
+                       (r'^~/$', viewWrapper(monkeySkull)),
+                       (r'^LOADING/$', viewWrapper(loadingCrazy)),
                        (r'^home/$', viewWrapper(home)),
                        (r'^machine_learning/$', viewWrapper(machine_learning)),
                        (r'^twitter_visualization/$', viewWrapper(twitter_visualization)),
@@ -35,8 +39,8 @@ urlpatterns += patterns('',
 #
 # # # redirect everything else
 urlpatterns += patterns('',
-                        (r'^/$',  redirect, {'page':"/home/"}),
-                        (r'^$',  redirect, {'page':"/home/"}),
+                        (r'^/$',  redirect, {'page':"/~/"}),
+                        (r'^$',  redirect, {'page':"/~/"}),
                         # (r'.*$',  redirect, {'page':"/home/"}),
                         ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
