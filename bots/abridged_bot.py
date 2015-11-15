35cdce2c9981e34d1d7ed02fa5489f611d545b30
Max Fowler <maxhfowler@gmail.com>
2015-11-15 16:59:43 -0500

    Touch up css

---
+++ bots/abridged_bot.py
@@ -0,0 +1,15 @@
+from settings.common import SECRETS_DICT
+
+import json
+import smtplib
+import urllib2
+import time
+import random
+import numpy
+
+from mhf.models import TwitterID
+
+
+
+if __name__ == '__main__':
+    print 'abridged bot'
